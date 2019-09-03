7622c46f640b0bec804c6a88f47f1dc2fd16c0e4
Marcelo <marcellopsnet@gmail.com>
2019-09-02 21:52:14 -0300

    Criação script popula tabela pessoa.

---
+++ projmps/src/main/resources/db/migration/V03__popula_tabela_pessoa.sql
@@ -0,0 +1,5 @@
+insert into  pessoa(nome, logradouro, bairro, complemento, numero, cep,  estado, cidade, ativo)
+ values ('João souza','Av. passa 4', 'Centro', 'Proximo a capela','50','76.620-000','São paulo','São paulo',true);
+
+insert into  pessoa(nome, logradouro, bairro, complemento, numero, cep,  estado, cidade, ativo)
+ values ('Paulo santos','Av. 88', 'Jardim novo mundo', 'Praça feliz','20','75.610-000','Rio de janeiro','Angra dos reis',true);
